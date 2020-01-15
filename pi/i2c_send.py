0da08a13d0d1fcb0a28924d05a65b9f43ff9bf16
Jakob <jakob.g.maier@hotmail.com>
2020-01-15 22:35:18 +0100

    udpate

--- pi/i2c_send.py
+++ pi/i2c_send.py
@@ -14,45 +14,45 @@ def parseDeparture(json_response):
 	return json_response['data']['monitors'][0]['lines'][0]['departures']['departure']	
 
 def parseDate(string):
-    return int(datetime.datetime.strptime(string, '%Y-%m-%dT%H:%M:%S').strftime("%s"))
+	return int(datetime.datetime.strptime(string, '%Y-%m-%dT%H:%M:%S').strftime("%s"))
 
 def writeNumber(value):
-    bus.write_byte(address, value)
-    return -1
+	bus.write_byte(address, value)
+	return -1
 
 def readNumber():
-    number = bus.read_byte(address)
-    return number
+	number = bus.read_byte(address)
+	return number
 
 current_timestamp = parseDate(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')) 
 
-for file in files:
+for count, file in enumerate(files):
 
-    with open(file) as f:
-        data = json.load(f)
-        f.close()
+	with open(file) as f:
+		data = json.load(f)
+		f.close()
 
-    countdown = -1
+	countdown = -1
 
-    for count, departure in enumerate(parseDeparture(data)):
-        timeRealReadable = str(departure['departureTime']['timeReal'])[:-9]
-        timeReal = parseDate(timeRealReadable)
+	for departure in parseDeparture(data):
+		timeRealReadable = str(departure['departureTime']['timeReal'])[:-9]
+		timeReal = parseDate(timeRealReadable)
 
-        if (current_timestamp <= timeReal):
-            countdown = (timeReal - current_timestamp) / 60
-            print("{} in {} minutes".format(file, countdown))
+		if (current_timestamp <= timeReal):
+			countdown = (timeReal - current_timestamp) / 60
+			print("{} in {} minutes".format(file, countdown))
 
-            if (countdown > 127): # can not be correctly encoded
-                break
-        
-            writeNumber(countdown | (count << 7))
-            print("Raspberry sends: {} to line {}".format(countdown, count))
-            time.sleep(1)
-            received = readNumber()
-            print("Arduino sends: ", received)
-            time.sleep(2)
-            break
-            
+			if (countdown > 127): # can not be correctly encoded
+				break
+
+			writeNumber(countdown | (count << 7))
+			print("Raspberry sends: {} to line {}".format(countdown, count))
+			time.sleep(1)
+			received = readNumber()
+			print("Arduino sends: {}".format(received))
+			time.sleep(2)
+			break
+
 
 
 
