e1982c4efdedf65051ca2453bb30bb57c613f9f1
Jakob <e11809618@student.tuwien.ac.at>
2020-01-15 20:15:53 +0100

    udpate

--- pi/i2c_send.py
+++ pi/i2c_send.py
@@ -1,16 +1,43 @@
 import smbus2
 import time
 import json
-bus = smbus2.SMBus(1)
+import datetime
+#bus = smbus2.SMBus(1)
 
 address = 0x05
+def parse_departure(json_response):
+	return json_response['data']['monitors'][0]['lines'][0]['departures']['departure']	
+
+
+def parse_date(string):
+    return int(datetime.datetime.strptime(string, '%Y-%m-%dT%H:%M:%S').strftime("%s"))
+
+
 
 with open("data.json") as f:
     data = json.load(f)
-    print(data)
+    f.close()
+
+
+current_timestamp = parse_date(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')) 
+
+
+for departure in parse_departure(data):
+    timeRealReadable = str(departure['departureTime']['timeReal'])[:-9]
+    timeReal = parse_date(timeRealReadable)
+
+    if (current_timestamp <= timeReal):
+        print("Next bim:")
+        print(timeRealReadable)
+        print("in x minutes:")
+        countdown = (timeReal - current_timestamp) / 60
+        print(countdown)
+        break
 
 
 
+
+"""
 def writeNumber(value):
     bus.write_byte(address, value)
     return -1
@@ -19,8 +46,6 @@ def readNumber():
     number = bus.read_byte(address)
     return number
 
-
-"""
 while True:
     send = input("Number between 1 - 9: ")
     if not send:
