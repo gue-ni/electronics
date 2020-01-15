07ff44ec481961ceb974b5f5ddef585ccc11927a
Jakob <e11809618@student.tuwien.ac.at>
2020-01-15 17:10:14 +0100

    update

---
+++ pi/i2c_send.py
@@ -0,0 +1,28 @@
+import smbus
+import time
+bus = smbus.SMBus(1)
+
+address = 0x05
+
+def writeNumber(value):
+    bus.write_byte(address, value)
+    return -1
+
+def readNumber():
+    number = bus.read_byte(address)
+    return number
+
+while True:
+    senden = input("Zahl zwischen 1 - 9: ")
+    if not senden:
+        continue
+
+    writeNumber(senden)
+    print("Raspberry Schickt folgende Zahl: ", senden)
+    time.sleep(1)
+
+    empfang = readNumber()
+    print("Der Arduino empfengt/schickt folgende Zahl: ", empfang)
+
+
+
