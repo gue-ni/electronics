8b3c1fd29120c533f46a3f28bf45d4465c73e41e
Jakob <e11809618@student.tuwien.ac.at>
2020-01-15 10:05:00 +0100

    update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Abfahrtsmonitor
 https://www.data.gv.at/katalog/api/3/action/package_show?id=add66f20-d033-4eee-b9a0-47019828e698
-
+https://www.data.gv.at/katalog/dataset/stadt-wien_wienerlinienechtzeitdaten
 
 https://www.wienerlinien.at/ogd_realtime/newsList?sender=
 https://www.wienerlinien.at/ogd_realtime/monitor?rbl=172&sender=
--- get_data.py
+++ get_data.py
@@ -4,11 +4,28 @@ import json
 base_url = "https://www.wienerlinien.at/ogd_realtime"
 monitor_url = "{}/monitor".format(base_url)
 
+def parse_status_code(code):
+	if code == "200":
+		return "OK"
+	if code == "311":
+		return "DB nicht verfuegbar"
+	if code == "312":
+		return "Haltepunkt existiert nicht"
+	if code == "316":
+		return "max. Anfragen ueberschritten"
+	if code == "317":
+		return "Sender existiert nicht"
+	if code == "320":
+		return "GET Anfrage Parameter ivalid"
+	
+	return "Unknown status code {}".format(code)
 
 rbl_pha = 165 # richtung prater hauptallee
 rbl_sfp = 172 # richtung stefan fadinger platz
 
-response = requests.get(monitor_url, params=[("rbl", 165), ("sender", "")])
+response = requests.get(monitor_url, params=[("rbl", rbl_pha)])
+
+print(parse_status_code(response.status_code))
 
 departures = response.json()['data']['monitors'][0]['lines'][0]['departures']['departure']
 
