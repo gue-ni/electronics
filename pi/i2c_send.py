df72c7e9d0b9346fb2438cccf744cfc2ffeefdc4
Jakob <jakob.g.maier@hotmail.com>
2020-01-15 20:36:53 +0100

    update

--- pi/i2c_send.py
+++ pi/i2c_send.py
@@ -39,7 +39,7 @@ for file in files:
 
         if (current_timestamp <= timeReal):
             countdown = (timeReal - current_timestamp) / 60
-            print("{file} in {} minutes".format(file, countdown))
+            print("{} in {} minutes".format(file, countdown))
         
             writeNumber(countdown)
             print("Raspberry sends: ", countdown)
