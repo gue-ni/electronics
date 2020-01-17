99767ccedf458a373b324ccc49a1a84476561e98
Jakob <e11809618@student.tuwien.ac.at>
2020-01-17 12:14:35 +0100

    update

--- arduino/serial_receive/serial_receive.ino
+++ arduino/serial_receive/serial_receive.ino
@@ -1,9 +1,20 @@
-lvoid setup() {
+
+
+int number;
+
+void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial
+  
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (Serial.available()){
+    number = Serial.read()
+    Serial.write(number);
+  }
 
 }
