b00c7b522e006efa8c05ff1a8d126a75c023cc46
Jakob <e11809618@student.tuwien.ac.at>
2020-01-17 19:18:06 +0100

    update

---
+++ pi/serial_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import serial
+import time
+import struct
+ 
+s = serial.Serial('/dev/ttyACM0', 9600) # Namen ggf. anpassen
+try:
+    s.open()
+except IOError:
+    s.close()
+    s.open()
+
+
+time.sleep(3) # der Arduino resettet nach einer Seriellen Verbindung, daher muss kurz gewartet werden
+ 
+to_send = struct.pack('B', 0x41)
+
+s.write(to_send)
+
+try:
+    while True:
+        response = s.read(1)
+#        print(type(response))
+
+#        response = int.from_bytes(response, byteorder='little', signed=False)
+        response = struct.unpack('B', response)[0]        
+        print(response)
+except KeyboardInterrupt:
+    s.close()
