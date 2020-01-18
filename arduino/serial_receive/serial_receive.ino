0769930205c988dca11571382d565722a7507a86
Jakob <e11809618@student.tuwien.ac.at>
2020-01-18 09:47:33 +0100

    update

--- arduino/serial_receive/serial_receive.ino
+++ arduino/serial_receive/serial_receive.ino
@@ -1,20 +1,15 @@
 
-
-int number;
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial
   
-
+void setup(){
+    Serial.begin(9600);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available()){
-    number = Serial.read()
-    Serial.write(number);
-  }
-
+ 
+void loop(){
+    if (Serial.available()) {
+        uint8_t nr = Serial.read();
+        //Serial.print("Folgender char wurde empfangen: ");
+        //Serial.println(nr, BIN);
+        Serial.write(nr);
+        Serial.write(nr+1);
+    }
 }
