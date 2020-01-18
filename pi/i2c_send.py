4ba6f61a38be7100edf83337011b343fcb60a94f
Jakob <jakob.g.maier@hotmail.com>
2020-01-18 12:25:30 +0100

    update

--- pi/i2c_send.py
+++ pi/i2c_send.py
@@ -16,32 +16,12 @@ time_format = "%Y-%m-%dT%H:%M:%S"
 
 files = ["data_172.json", "data_165.json"]
 
-'''
-def i2c_send(number, line):
-    if (number > 127): # can not be correctly encoded
-        print("number to large")
-        return
-
-    
-    data = struct.pack('BBBB', 0xf, 0xff, 0x41, 0x54)
-    #bus.write_byte(address, number | (line << 7))
-    bus.write_i2c_block_data(address, i2c_cmd_write, data)
-    time.sleep(1)
-    received = bus.read_byte(address) 
-
-    if (countdown != received):
-        print("error sending/receiving")
-
-'''
-
 def parse_time(now, dep):
     timeReal = str(dep['departureTime']['timeReal'])[:-9]
     timeReal = datetime.strptime(timeReal, time_format) 
-    c =  int(round((timeReal - timeNow).seconds / 60.0))
-    print(c)
-    return c
+    return int(round((timeReal - timeNow).seconds / 60.0))
 
-times = []
+countdown = []
 
 for line, file in enumerate(files):
 
@@ -65,14 +45,12 @@ for line, file in enumerate(files):
             #print(departures[i+1]['departureTime'])
             n1 = parse_time(timeNow, departures[i])
             n2 = parse_time(timeNow, departures[i+1])
-            times.append(n1)
-            times.append(n2)
+            countdown.append(n1)
+            countdown.append(n2)
 
-            print("[{}] Towards {} in {} and {} minutes".format(timeNow, direction, n1, n2))
+            print("[{}] Station {} towards {} in {} and {} minutes".format(timeNow, stop, direction, n1, n2))
             break
 
-print(times)
-bus.write_i2c_block_data(address, i2c_cmd_write, times)
+bus.write_i2c_block_data(address, i2c_cmd_write, countdown)
 time.sleep(1)
 response  = bus.read_i2c_block_data(address, i2c_cmd_read, 5)
-print(response)
